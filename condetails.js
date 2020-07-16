417deb7d57bb76dd6b206f5775e816cefd4161f9
Karthick Bharathwaj <karthickbharathwaj.g@tcs.com>
2020-07-16 14:05:26 +0530

    new changes

--- condetails.js
+++ condetails.js
@@ -1,18 +1,37 @@
-$(document).ready(function () {
-    $.ajax({
-        url: 'https://morning-wildwood-74823.herokuapp.com/api/contact',
-        type: 'GET',
-        dataType: 'json',
-        async: true,
-        success: function (data, textStatus, xhr) {
-            var trHTML = '';
-            $.each(data, function (i, item) {
-                trHTML += '<tr><td>' + item.name + '</td><td>' + item.email + '</td><td>' + item.subject + '</td><td>' + item.message + '</td></tr>';
-            });
-            $('#records_table').append(trHTML);
+// $(document).ready(function () {
+//     $.ajax({
+//         url: 'https://morning-wildwood-74823.herokuapp.com/api/contact',
+//         type: 'GET',
+//         dataType: 'json',
+//         async: true,
+//         success: function (data, textStatus, xhr) {
+//             var trHTML = '';
+//             $.each(data, function (i, item) {
+//                 trHTML += '<tr><td>' + item.name + '</td><td>' + item.email + '</td><td>' + item.subject + '</td><td>' + item.message + '</td></tr>';
+//             });
+//             $('#records_table').append(trHTML);
+//         },
+//         error: function (xhr, textStatus, errorThrown) {
+//             console.log('Error in Operation');
+//         }
+//     });
+// });
+$(function () {
+    $('#dataTable').DataTable({
+        "ajax": {
+            "url": "https://morning-wildwood-74823.herokuapp.com/api/contact", /*Data source*/
+            "dataSrc": "data", /*object that holds the data*/
         },
-        error: function (xhr, textStatus, errorThrown) {
-            console.log('Error in Operation');
-        }
+        columns: [
+            { data: 'name' },
+            { data: 'email' },
+            { data: 'subject' },
+            { data: 'message' }
+            
+        ],
+        "columnDefs": [{ /* default values for columns */
+            "defaultContent": "-",
+            "targets": "_all"
+        }],
     });
 });
--- organization.js
+++ organization.js
@@ -1,18 +1,37 @@
-$(document).ready(function () {
-    $.ajax({
-        url: 'https://morning-wildwood-74823.herokuapp.com/api/organization',
-        type: 'GET',
-        dataType: 'json',
-        async: true,
-        success: function (data, textStatus, xhr) {
-            var trHTML = '';
-            $.each(data, function (i, item) {
-                trHTML += '<tr><td>' + item.type + '</td><td>' + item.email + '</td><td>' + item.service+ '</td><td>' + item.about_organization + '</td></tr>';
-            });
-            $('#records_table').append(trHTML);
+// $(document).ready(function () {
+//     $.ajax({
+//         url: 'https://morning-wildwood-74823.herokuapp.com/api/organization',
+//         type: 'GET',
+//         dataType: 'json',
+//         async: true,
+//         success: function (data, textStatus, xhr) {
+//             var trHTML = '';
+//             $.each(data, function (i, item) {
+//                 trHTML += '<tr><td>' + item.type + '</td><td>' + item.email + '</td><td>' + item.service+ '</td><td>' + item.about_organization + '</td></tr>';
+//             });
+//             $('#records_table').append(trHTML);
+//         },
+//         error: function (xhr, textStatus, errorThrown) {
+//             console.log('Error in Operation');
+//         }
+//     });
+// });  
+$(function () {
+    $('#dataTable').DataTable({
+        "ajax": {
+            "url": "https://morning-wildwood-74823.herokuapp.com/api/organization", /*Data source*/
+            "dataSrc": "data", /*object that holds the data*/
         },
-        error: function (xhr, textStatus, errorThrown) {
-            console.log('Error in Operation');
-        }
+        columns: [
+            { data: 'type' },
+            { data: 'email' },
+            { data: 'service' },
+            { data: 'about_organization' }
+            
+        ],
+        "columnDefs": [{ /* default values for columns */
+            "defaultContent": "-",
+            "targets": "_all"
+        }],
     });
-});  
+});
